d6da6cd177034cf61d10e135ce962062b0e10f68
x3mart <x3mart@gmail.com>
2026-07-21 17:18:23 +0300

    source folder

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).parent.absolute()
+
 HOME = Path.home()
-SOURCE_FOLDER = HOME / "voyra-wind-service" /"wind-data-service" / "data"
+SOURCE_FOLDER = PROJECT_ROOT.parent /"wind-data-service" / "data"
 NPZ_FILE_NAME = "windgrid.npz"
 CURRENT_CYCLE_JSON_PATH = SOURCE_FOLDER / "current_cycle.json"
 NPZ_PATH = SOURCE_FOLDER / NPZ_FILE_NAME
